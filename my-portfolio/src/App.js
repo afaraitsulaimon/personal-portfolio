b722b67c0faec813f95455ad3d757f9b57a223a9
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-06-04 17:38:57 +0100

    completed responsive menu

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
+function App() {
+  return (
+    <div className="App" >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='about-me' element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/components/Menu.js
@@ -0,0 +1,112 @@
+import { Button } from '@mui/material';
+import React, { useEffect, useState} from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FaBars, FaTimes } from "react-icons/fa";
+import './MenuDisplay.css'
+
+
+const Menu = () => {
+
+    const [showHideResponsive, setShowHideResponsive] = useState(true);
+    const [displayMenu, setDisplayMenu] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth)
+   
+
+
+    const handleResponseMenu = () => {
+        setShowHideResponsive(!showHideResponsive);
+        setDisplayMenu(!displayMenu);
+
+}
+
+const checkWidth = () => {
+    setWidth(window.innerWidth);
+    
+}
+   
+useEffect(() => {
+    window.addEventListener('resize', checkWidth);
+    if (width >= 768) {
+        setDisplayMenu(false);
+        setShowHideResponsive(true);
+
+    }
+},[width])
+
+
+  return (
+  
+    <Header>
+
+            <MenuLogo>
+               <Link to='/'> <h1>ASB</h1></Link>
+            </MenuLogo>
+
+            <div className={displayMenu ? `showLinksForSmallScreen` : `hideLinksForSmallScreen`}>
+
+                    <ul className='nav-links'>
+                        <li>
+                            <Link to='/'>About</Link>
+                        </li>
+
+                        <li>
+                            <Link to='/'>Portfolio</Link>
+                        </li>
+
+                        <li>
+                            <Link to='/'>Resume</Link>
+                        </li>
+                    </ul>
+
+                <div>
+                    <Button variant="contained">Get in Touch</Button>
+                </div>  
+
+            </div>
+
+                <ResponsiveDisplayButton onClick={handleResponseMenu}>
+                    {showHideResponsive ?  <FaBars/>: <FaTimes/>}
+                </ResponsiveDisplayButton>
+   
+
+            
+    </Header>
+  )
+}
+
+
+const Header = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    background-color:green;
+    font-family:Montserrat, san-serif;
+    max-height:10vh;
+
+    
+`;
+
+const MenuLogo = styled.div`
+    a{
+        color:white;
+        text-decoration:none;
+    }
+`;
+
+
+
+
+
+
+const ResponsiveDisplayButton = styled.div`
+        cursor:pointer;
+        display:none;
+
+        @media(max-width:768px){
+
+            display:block;
+        }
+`;
+
+export default Menu
