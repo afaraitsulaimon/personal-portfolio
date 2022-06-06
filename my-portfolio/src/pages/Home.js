7bcaa11d840e8ef35ea39b37c7f7e6645ad74155
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-06-06 11:29:59 +0100

    footer designed

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from '../components/Menu'
+import Footer from './Footer'
 
 const Home = () => {
   return (
     <div>
         <Menu/>
+        <Footer/>
     </div>
   )
 }
